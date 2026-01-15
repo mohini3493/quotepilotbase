0ffae6b9f21fc3551895beed93535dd25b191ab5
mohini3493 <mohini3493@gmail.com>
2026-01-15 15:37:51 +0530

    changed port api railway

--- api/src/index.ts
+++ api/src/index.ts
@@ -40,7 +40,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/products/admin/all", adminProductRoutes);
 
-const PORT = Number(process.env.PORT) || 4000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`API running on http://0.0.0.0:${PORT}`);
