a20b1f1cd14af058ad732b3439bbb5dea3acc672
mohini3493 <mohini3493@gmail.com>
2026-02-20 09:50:05 +0530

    updated index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -25,6 +25,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
   "https://quote-front-production.up.railway.app",
+  "https://infinityglazing.com",
 ];
 
 app.use(
